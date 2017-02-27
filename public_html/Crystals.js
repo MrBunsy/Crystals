a6386e0d3eccb14ad0a18234db235f336855ba8f
lw <luke.wallin@roke.co.uk>
2017-02-27 16:18:48 +0000

    can generate particles in random positions

--- public_html/Crystals.js
+++ public_html/Crystals.js
@@ -1,9 +1,11 @@
 var Crystals = function (div) {
-    var self=this;
-    this.div = div
+    var self = this;
+    this.div = div;
     this.width = parseInt(this.div.style.width);
     this.height = parseInt(this.div.style.height);
     this.pos = new Vector(this.div.offsetLeft, this.div.offsetTop);
+    this.particles = [];
+
 
     this.canvas = document.createElement("canvas");
 //    this.canvases[i].style.position = "absolute";
@@ -12,7 +14,7 @@ var Crystals = function (div) {
 
     this.div.appendChild(this.canvas);
     this.ctx = this.canvas.getContext("2d");
-    
+
 
     this.fps = 60;
 
@@ -25,12 +27,47 @@ var Crystals = function (div) {
         }, this.interval);
     };
 
-    this.update = function () {
-        //main loop
-        this.ctx.clearRect(0,0,this.width+1,this.height+1);
-        this.ctx.beginPath();
-        this.ctx.arc(95,50,40,0,Math.PI);
-        this.ctx.stroke();
+    this.metresToPixels = function (metresVector, offsetInMetres, pixelsPerMetre) {
+        return (metresVector.subtract(offsetInMetres)).multiply(pixelsPerMetre)
     };
 
-}
+    this.draw = function (offsetInMetres, pixelsPerMetre) {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+
+        for (var i = 0; i < this.particles.length; i++) {
+            screenPos = this.metresToPixels(this.particles[i].position, offsetInMetres, pixelsPerMetre)
+
+            this.ctx.beginPath();
+            this.ctx.arc(screenPos.x, screenPos.y, 1, 0, Math.PI * 2);
+            this.ctx.fill();
+
+
+        }
+
+
+
+    };
+
+    this.update = function () {
+        //main loop
+        this.draw(new Vector(0, 0), 10);
+    };
+
+    this.randomPosition = function () {
+        return new Vector(Math.random() - 0.5, Math.random() - 0.5);
+    }
+
+    this.spawnParticles = function (num, maxDistance) {
+        if (typeof (maxDistance) === "undefined") {
+            maxDistance = 100;
+        }
+        this.particles = [];
+        for (var i = 0; i < num; i++) {
+            this.particles.push(new Particle(this.randomPosition().multiply(maxDistance)))
+        }
+    };
+};
+
+var Particle = function (position) {
+    this.position = position;
+};
